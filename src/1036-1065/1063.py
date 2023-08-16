aa100d1a223bd437c0900310ad901faf47106940
Romeo Ahmed <ahmedorqwn@gmail.com>
2023-08-16 17:20:38 +0800

    Update 1063.py

--- src/1036-1065/1063.py
+++ src/1036-1065/1063.py
@@ -24,14 +24,12 @@ limitations under the License.
 @Desc    :   None
 """
 
-from math import sqrt, floor
-
 def isPrimeNum(n):
     if (n == 2) or (n == 3):
         return True
     elif (n % 6 != 1) and (n % 6 != 5):
         return False
-    for i in range(5, floor(sqrt(n)) + 1, 6):
+    for i in range(5, int(n ** 0.5) + 1, 6):
         if (n % i == 0) or (n % (i + 2) == 0):
             return False
     return True
