a57fef4989767f006444e36319f9ef8f60877ebd
Romeo Ahmed <ahmedorqwn@gmail.com>
2023-08-16 17:45:28 +0800

    Added some source files

---
+++ src/1079-1093/1082.py
@@ -0,0 +1,34 @@
+"""
+Copyright 2023 Romeo Ahmed <ahmedorqwn@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   1082.py
+@Time    :   2023/08/16 17:26:38
+@Author  :   romeoahmed 
+@Version :   1.0
+@Desc    :   None
+"""
+
+from math import sqrt
+l1, l2, l3, l4, l5 = map(float, input().split())
+
+p1 = (l1 + l2 + l5) / 2
+p2 = (l3 + l4 + l5) / 2
+s = sqrt(p1*(p1-l1)*(p1-l2)*(p1-l5)) + sqrt(p2*(p2-l3)*(p2-l4)*(p2-l5))
+
+print("%.2f" % s)
---
+++ src/1079-1093/1083.py
@@ -0,0 +1,45 @@
+"""
+Copyright 2023 Romeo Ahmed <ahmedorqwn@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   1083.py
+@Time    :   2023/08/16 17:27:57
+@Author  :   romeoahmed 
+@Version :   1.0
+@Desc    :   None
+"""
+
+m, n = map(int, input().split())
+facts_m = []
+facts_n = []
+
+for i in range(1, m + 1):
+    if m % i == 0:
+        facts_m.append(i)
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        facts_n.append(i)
+
+num = 1
+for i in facts_m:
+    if i > num and i in facts_n:
+        num = i
+
+result = int(m * n / num)
+print(result)
