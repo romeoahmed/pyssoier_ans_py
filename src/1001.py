f1386580a15fc1b50f3bc8cc77c0034299cf1cce
Romeo Ahmed <ahmedorqwn@gmail.com>
2023-08-16 11:26:36 +0800

    Added some source files

---
+++ src/1001.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File    :   1001.py
+@Time    :   2023/08/16 11:18:16
+@Author  :   romeoahmed 
+@Version :   1.0
+@Desc    :   None
+'''
+
+print("Hello World")
---
+++ src/1002.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File    :   1002.py
+@Time    :   2023/08/16 11:18:00
+@Author  :   romeoahmed 
+@Version :   1.0
+@Desc    :   None
+'''
+
+a = int(input())
+b = int(input())
+print(a + b)
