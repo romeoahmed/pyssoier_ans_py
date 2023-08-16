72c72d838af9875c107b50eda913270568e7672f
Romeo Ahmed <ahmedorqwn@gmail.com>
2023-08-16 14:47:29 +0800

    Changed some source files

--- src/1007-1024/1023.py
+++ src/1007-1024/1023.py
@@ -1,12 +1,28 @@
+"""
+Copyright 2023 Romeo Ahmed <ahmedorqwn@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
+"""
 @File    :   1023.py
 @Time    :   2023/08/16 13:36:33
 @Author  :   romeoahmed 
 @Version :   1.0
 @Desc    :   None
-'''
+"""
 
 l = list(map(int, input().split()))
 for i in range(len(l)):
@@ -24,4 +40,4 @@ for i in range(len(l)):
         l[i-1] += l[i]
 
 for j in l:
-    print("%5d" % j, end='')
+    print("%5d" % j, end="")
