62dd1e9f6a36b3ee10f5139582e481bddd58d318
romeoahmed <ahmedorqwn@gmail.com>
2023-08-22 20:56:21 +0800

    Update rmFileInfo.py

--- src/rmFileInfo.py
+++ src/rmFileInfo.py
@@ -20,9 +20,9 @@ limitations under the License.
 import os
 
 def rmFileInfo(path):
-    with open(path, 'r') as r:
+    with open(path, 'r', encoding="utf-8") as r:
         lines = r.readlines()
-    with open (path, 'w') as w:
+    with open (path, 'w', encoding="utf-8") as w:
         mark = False
         for i in range(0, len(lines)):
             if ("\"\"\"" in lines[i]) and ("@File" in lines[i + 1]):
