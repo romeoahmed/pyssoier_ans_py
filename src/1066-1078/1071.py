093e9cbfa6f894e1fa1d328a046c5b91d341d3e9
romeoahmed <ahmedorqwn@gmail.com>
2023-08-24 23:16:18 +0800

    Update 1071.py

--- src/1066-1078/1071.py
+++ src/1066-1078/1071.py
@@ -8,17 +8,9 @@ Licensed under the MIT license. See LICENSE file in the project root for license
 
 matches = (6, 2, 5, 5, 4, 5, 6, 3, 7, 6)
 for i in range(112):
-    if i < 10:
-        if matches[i] == 6:
-            print(i)
-    elif 10 <= i < 100:
-        x = i // 10
-        y = i % 10
-        if matches[x] + matches[y] == 6:
-            print(i)
-    else:
-        x = i // 100
-        y = (i // 10) % 10
-        z = i - 100 * x - 10 * y
-        if matches[x] + matches[y] + matches[z] == 6:
-            print(i)
+    if i < 10 and matches[i] == 6:
+        print(i)
+    elif 10 <= i < 100 and matches[i // 10] + matches[i % 10] == 6:
+        print(i)
+    elif i >= 100 and matches[i // 100] + matches[(i // 10) % 10] + matches[i % 10] == 6:
+        print(i)
