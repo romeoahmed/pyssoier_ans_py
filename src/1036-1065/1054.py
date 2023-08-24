40c5dbe0ab6c453e3422e3df30bf3fb3a2ca7fa1
romeoahmed <ahmedorqwn@gmail.com>
2023-08-24 15:41:38 +0800

    Update 1054.py

--- src/1036-1065/1054.py
+++ src/1036-1065/1054.py
@@ -7,19 +7,7 @@ Licensed under the MIT license. See LICENSE file in the project root for license
 # -*- coding: utf-8 -*-
 
 m, n = map(int, input().split())
-facts_m = []
-facts_n = []
-
-for i in range(1, m + 1):
-    if m % i == 0:
-        facts_m.append(i)
-for i in range(1, n + 1):
-    if n % i == 0:
-        facts_n.append(i)
-
-result = 1
-for i in facts_m:
-    if i > result and i in facts_n:
-        result = i
-
-print(result)
+for i in range(min(m, n), 0, -1):
+    if m % i == 0 and n % i == 0:
+        print(i)
+        break
