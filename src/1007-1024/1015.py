0e23adea496215253dbdab1767fa32cd2129eb92
Romeo Ahmed <ahmedorqwn@gmail.com>
2023-08-24 14:57:00 +0800

    Update 1015.py

--- src/1007-1024/1015.py
+++ src/1007-1024/1015.py
@@ -7,8 +7,10 @@ Licensed under the MIT license. See LICENSE file in the project root for license
 # -*- coding: utf-8 -*-
 
 num = int(input())
+
 a = num // 100
 b = num // 10 - a * 10
 c = num - (num // 10) * 10
 new = c * 100 + b * 10 + a
+
 print(new)
