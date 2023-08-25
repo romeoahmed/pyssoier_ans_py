a0862bacde1ec8abd206544585725903fe9f9a27
romeoahmed <ahmedorqwn@gmail.com>
2023-08-25 11:16:07 +0800

    Update 1078.py

--- src/1066-1078/1078.py
+++ src/1066-1078/1078.py
@@ -7,13 +7,13 @@ Licensed under the MIT license. See LICENSE file in the project root for license
 # -*- coding: utf-8 -*-
 
 n, m = map(int, input().split())
-persons = [str(i) for i in range(1, n + 1)]
-out_persons = []
-del_num = m - 1
+persons = [i for i in range(1, n + 1)]
+outPersons = []
+delNum = m - 1
 for i in range(n):
-    out_persons.append(persons[del_num])
-    del persons[del_num]
+    outPersons.append(persons[delNum])
+    del persons[delNum]
     if len(persons) != 0:
-        del_num = (del_num + m - 1) % len(persons)
+        delNum = (delNum + m - 1) % len(persons)
 
-print(*out_persons, sep=" ")
+print(*outPersons, sep=" ")
