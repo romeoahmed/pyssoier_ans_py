525979853a7be48cabda2b058e40548c158884a1
romeoahmed <ahmedorqwn@gmail.com>
2023-08-30 15:22:17 +0800

    Update 1083.py

--- src/1079-1093/1083.py
+++ src/1079-1093/1083.py
@@ -6,22 +6,13 @@ Licensed under the MIT license. See LICENSE file in the project root for license
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+def getGCD(m, n):
+    return m if n == 0 else getGCD(n, m % n)
+
+def getLCM(m, n):
+    return m * n / getGCD(m, n)
+
 m, n = map(int, input().split())
-facts_m = []
-facts_n = []
+result = getLCM(m, n)
 
-for i in range(1, m + 1):
-    if m % i == 0:
-        facts_m.append(i)
-
-for i in range(1, n + 1):
-    if n % i == 0:
-        facts_n.append(i)
-
-num = 1
-for i in facts_m:
-    if i > num and i in facts_n:
-        num = i
-
-result = int(m * n / num)
 print(result)
